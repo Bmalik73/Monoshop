eab45cdc57a7b1004efa5910aa6bcca2688083ab
Bmalik73 <bmalik01@live.fr>
2023-01-21 14:47:20 +0100

    display product

--- config/commandes.php
+++ config/commandes.php
@@ -12,4 +12,21 @@
         }
     }
 
+    function afficher()
+    {
+        if (require(connexion.php)) 
+        {
+            $req=$access->prepare("SELECT * FROM produits ORDER BY id DESC");
+
+            $req->execute();
+
+            $data = $req->fetchAll(PDO::FETCH_OBJ);
+
+            return $data;
+
+            $req->closeCursor();
+        }
+
+    }
+
 ?>
